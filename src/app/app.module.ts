1854e8d7148118c58be7da29a5008b5501d8d0a6
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-18 13:22:59 -0300

    Imports to be used in the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// To work with forms in Angular 18
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+// To make HTTP requests
+import { HttpClientModule } from '@angular/common/http';
+
+//Imports for Angular Material components
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +35,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserAnimationsModule,
+      // Forms
+      FormsModule,
+      ReactiveFormsModule,
+      // Requests http
+      HttpClientModule,
+      // Angular Material
+      MatFormFieldModule,
+      MatPaginatorModule,
+      MatCheckboxModule,
+      MatSnackBarModule,
+      MatToolbarModule,
+      MatSidenavModule,
+      MatButtonModule,
+      MatSelectModule,
+      MatInputModule,
+      MatRadioModule,
+      MatTableModule,
+      MatIconModule,
+      MatListModule,
+      MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
