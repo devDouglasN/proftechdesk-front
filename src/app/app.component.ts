e58d327ff126686bc1f33ddc79cebf7bc331e571
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-18 12:54:18 -0300

    inlineTemplate and style disabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: []
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   title = 'proftechdesk-front';
