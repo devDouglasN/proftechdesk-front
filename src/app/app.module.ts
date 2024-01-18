e80c1543909ea5ec7349d8db72d9ce2080ad482d
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-18 13:40:36 -0300

    NAV component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,10 +27,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 
+// Project component
+import { NavComponent } from './components/nav/nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
