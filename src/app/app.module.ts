8ada42ee89bb707b690f1a4e1a5295e8ee379efc
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-19 12:46:38 -0300

    Home component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,12 +29,14 @@ import { MatCardModule } from '@angular/material/card';
 
 // Project component
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
