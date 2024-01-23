ad9a2fe5fb8062102d1db7f8e4ec615cd5dae985
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-23 19:13:16 -0300

    Add technicians table with pagination

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -10,6 +10,6 @@ export class NavComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.router.navigate(['home'])
+    this.router.navigate(['technicians'])
   }
 }
--- src/app/components/technician/technician-list/technician-list.component.ts
+++ src/app/components/technician/technician-list/technician-list.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Technical } from '../../../models/technical';
+
 
 @Component({
   selector: 'app-technician-list',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrl: './technician-list.component.css'
 })
 export class TechnicianListComponent {
+  ELEMENT_DATA: Technical[] = [
+    {
+      id: 1,
+      name: 'Douglas',
+      cpf: '335.437.860-32',
+      email: 'douglas@.com',
+      password: '1234',
+      profiles: ['0'],
+      creationDate: '15/08/2023'
+    }
+  ]
 
+  displayedColumns: string[] = ['id', 'name', 'cpf', 'email', 'action'];
+  dataSource = new MatTableDataSource<Technical>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
+
+
