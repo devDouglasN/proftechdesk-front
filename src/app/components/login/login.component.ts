17f8c09d5ede95205fc3bdea61dfed384256328f
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-01-25 16:41:54 -0300

    Updated: login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Credentials } from '../../models/credentials';
+import { FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  creds: Credentials = {
+    email: '',
+    password: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  password = new FormControl(null, Validators.minLength(3))
 
 }
