65434d893f09e58df75073dc7c2a9abf8929f1b8
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-02-02 21:24:32 -0300

    Logout

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,7 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Toast, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +9,20 @@ import { Router } from '@angular/router';
   styleUrl: './nav.component.css'
 })
 export class NavComponent implements OnInit {
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private toast: ToastrService) { }
 
   ngOnInit(): void {
     this.router.navigate(['technical'])
   }
+
+  logout() {
+    this.router.navigate(['login'])
+    this.authService.logout()
+    this.toast.info('Logout successful!', 'Logout', { timeOut: 7000 })
+  }
 }
+
+
