e995b9f7cd63a54c60b71577d6993715b07cd1c8
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-04-27 11:30:34 -0300

    Completion of technical creation

--- src/app/components/technician/technical-create/technical-create.component.ts
+++ src/app/components/technician/technical-create/technical-create.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Technical } from '../../../models/technical';
+import { TechnicalService } from '../../../services/technical.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-technical-create',
@@ -6,16 +11,52 @@ import { Component } from '@angular/core';
   styleUrl: './technical-create.component.css'
 })
 export class TechnicalCreateComponent {
-  technical: any;
+
+  technical: Technical = {
+    id: '',
+    name: '',
+    cpf: '',
+    email: '',
+    password: '',
+    profiles: [],
+    creationDate: ''
+  }
+
+  name: FormControl = new FormControl(null, Validators.minLength(3));
+  cpf: FormControl = new FormControl(null, Validators.required);
+  email: FormControl = new FormControl(null, Validators.email);
+  password: FormControl = new FormControl(null, Validators.minLength(3));
+
+  constructor(
+    private service: TechnicalService,
+    private toast: ToastrService,
+    private router: Router
+  ) {}
+
+  create(): void {
+    this.service.create(this.technical).subscribe(() => {
+      this.toast.success('Técnico cadastrado com sucesso', 'Cadastro');
+      this.router.navigate(['technical']);
+    }, ex => {
+      console.log(ex)
+      if(ex.error.status === 500){
+        this.toast.error('Número do Cadastro de Pessoa Física (CPF) brasileiro inválido')
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
 
   addProfile(profile: any): void {
-
     if(this.technical.profiles.includes(profile)){
       this.technical.profiles.splice(this.technical.profiles.indexOf(profile), 1)
     } else {
       this.technical.profiles.push(profile);
     }
-
   }
 
+  fieldValidate(): boolean {
+    return this.name.valid && this.cpf.valid
+     && this.email.valid && this.password.valid;
+  }
 }
--- src/app/services/technical.service.ts
+++ src/app/services/technical.service.ts
@@ -13,6 +13,11 @@ export class TechnicalService {
   constructor(private http: HttpClient) { }
 
   findAll(): Observable<Technical[]> {
-    return this.http.get<Technical[]>(`${API_CONFIG.baseUrl}/technical`);
+    return this.http.get<Technical[]>(`${API_CONFIG.baseUrl}/technicals`);
   }
+
+  create(technical: Technical): Observable<Technical> {
+    return this.http.post<Technical>(`${API_CONFIG.baseUrl}/technicals`, technical);
+  }
+
 }
