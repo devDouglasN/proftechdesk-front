7593a1a67723759ffcad08cd78d38a1fb0286f45
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-03 19:45:16 -0300

    Technical delete created

---
+++ src/app/components/technician/technical-delete/technical-delete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technical-delete',
+  templateUrl: './technical-delete.component.html',
+  styleUrl: './technical-delete.component.css'
+})
+export class TechnicalDeleteComponent {
+
+}
