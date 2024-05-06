025624c5724abb681ca34e0bc7e732c033f5bdef
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-06 11:06:44 -0300

    ticket-update component created

---
+++ src/app/components/ticket/ticket-update/ticket-update.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-update',
+  templateUrl: './ticket-update.component.html',
+  styleUrl: './ticket-update.component.css'
+})
+export class TicketUpdateComponent {
+
+}
