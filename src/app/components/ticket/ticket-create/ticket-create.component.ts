892ed5283ddce1ad4d7047a15fd1fa650b8b60ff
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-06 09:37:08 -0300

    Form validation for ticket-create

--- src/app/components/ticket/ticket-create/ticket-create.component.ts
+++ src/app/components/ticket/ticket-create/ticket-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ticket-create',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './ticket-create.component.css'
 })
 export class TicketCreateComponent {
+ticket: any;
+customer: any;
+technical: any;
+create() {
+throw new Error('Method not implemented.');
+}
+
+  title: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  priority: FormControl = new FormControl(null, [Validators.required])
+  technicianName: FormControl = new FormControl(null, [Validators.required])
+  customerName: FormControl = new FormControl(null, [Validators.required])
+  observations: FormControl = new FormControl(null, [Validators.required])
+
+  constructor(
+  ) {
+
+  }
+
+  fieldValidate(): boolean {
+    return this.title.valid &&
+      this.status.valid &&
+      this.priority.valid &&
+      this.technicianName &&
+      this.customerName.valid &&
+      this.observations.valid
+  }
 
 }
