5bbb8bf7148704e364b011c49631153ce01afef2
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-06 07:20:10 -0300

    Filter and findAll method for ticket

--- src/app/components/ticket/ticket-list/ticket-list.component.ts
+++ src/app/components/ticket/ticket-list/ticket-list.component.ts
@@ -9,6 +9,8 @@ import { MatPaginator } from '@angular/material/paginator';
   styleUrl: './ticket-list.component.css'
 })
 export class TicketListComponent {
+  service: any;
+
 applyFilter($event: KeyboardEvent) {
 throw new Error('Method not implemented.');
 }
@@ -20,5 +22,51 @@ throw new Error('Method not implemented.');
   dataSource = new MatTableDataSource<Ticket>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
+  findAll(): void {
+    this.service.findAll().subscribe(response => {
+      this.ELEMENT_DATA = response
+      this.dataSource = new MatTableDataSource<Ticket>(response);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  statusReturn(status: any): string {
+    if (status == '0') {
+      return 'ABERTO';
+    } else if (status == '1') {
+      return 'EM ANDAMENTO';
+    } else {
+      return 'ENCERRADO';
+    }
+  }
+
+  statusPriority(priority: any): string {
+    if (priority == '0') {
+      return 'BAIXA';
+    } else if (priority == '1') {
+      return 'MEDIA';
+    } else {
+      return 'ALTA';
+    }
+  }
+
+  orderByStatus(status: any): void {
+    let list: Ticket[] = []
+    this.ELEMENT_DATA.forEach(element => {
+      if (element.status == status)
+        list.push(element)
+    });
+    if (status !== '') {
+      this.FILTERED_DATA = list;
+      this.dataSource = new MatTableDataSource<Ticket>(list);
+      this.dataSource.paginator = this.paginator;
+    } else {
+      this.dataSource = new MatTableDataSource<Ticket>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
 
 }
