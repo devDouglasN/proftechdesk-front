6dd55669a725eff25963f4bdfba456d09e99817f
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-06 10:55:21 -0300

    Service to create ticket implemented

--- src/app/components/ticket/ticket-create/ticket-create.component.ts
+++ src/app/components/ticket/ticket-create/ticket-create.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Customer } from '../../../models/customer';
+import { Technical } from '../../../models/technical';
+import { TicketService } from '../../../services/ticket.service';
+import { CustomerService } from '../../../services/customer.service';
+import { TechnicalService } from '../../../services/technical.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { Ticket } from '../../../models/ticket';
 
 @Component({
   selector: 'app-ticket-create',
@@ -7,12 +15,20 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrl: './ticket-create.component.css'
 })
 export class TicketCreateComponent {
-ticket: any;
-customer: any;
-technical: any;
-create() {
-throw new Error('Method not implemented.');
-}
+
+ticket: Ticket = {   
+    priority:         '',      
+    status:           '',         
+    title:            '',          
+    observations:     '',   
+    technical:        '',      
+    customer:         '',       
+    technicianName:   '', 
+    customerName:     '', 
+  }
+
+  customer: Customer[] = []
+  technical: Technical[] = []
 
   title: FormControl = new FormControl(null, [Validators.required])
   status: FormControl = new FormControl(null, [Validators.required])
@@ -22,8 +38,40 @@ throw new Error('Method not implemented.');
   observations: FormControl = new FormControl(null, [Validators.required])
 
   constructor(
+    private ticketService:      TicketService,
+    private customerService:    CustomerService,
+    private technicalService:   TechnicalService,
+    private toast:              ToastrService,
+    private router:             Router,
   ) {
 
+  }
+
+  ngOnInit(): void {
+    this.findAllCustomer();
+    this.findAllTechnical();
+  }
+
+  create(): void {
+    this.ticketService.create(this.ticket).subscribe(response => {
+      this.toast.success('Chamado criado com sucesso', 'Cadastro');
+      this.router.navigate(['ticket'])
+    }, ex => {
+      console.log(ex)
+      this.toast.error(ex.error.error);
+    })
+  }
+
+  findAllCustomer(): void {
+    this.customerService.findAll().subscribe(response => {
+      this.customer = response
+    })
+  }
+
+  findAllTechnical(): void {
+    this.technicalService.findAll().subscribe(response => {
+      this.technical = response;
+    })
   }
 
   fieldValidate(): boolean {
