a6ea4e6daac5468157ea75ff7417774dc1498b4f
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-07 11:52:24 -0300

    Navigate to home page when starting

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -15,13 +15,13 @@ export class NavComponent implements OnInit {
     private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['/read'])
+    this.router.navigate(['home'])
   }
 
   logout() {
     this.router.navigate(['login'])
     this.authService.logout()
-    this.toast.info('Logout successful!', 'Logout', { timeOut: 7000 })
+    this.toast.info('Logout successful!', 'Logout', { timeOut: 3500 })
   }
 }
 
