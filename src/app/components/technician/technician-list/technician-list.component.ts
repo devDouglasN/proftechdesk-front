8efbc82c0aa0205bb4ccf72bf4f5caece2ec4372
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-24 16:07:49 -0300

    New page of technicians

--- src/app/components/technician/technician-list/technician-list.component.ts
+++ src/app/components/technician/technician-list/technician-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Technical } from '../../../models/technical';
@@ -10,7 +10,12 @@ import { TechnicalService } from '../../../services/technical.service';
   templateUrl: './technician-list.component.html',
   styleUrl: './technician-list.component.css'
 })
-export class TechnicianListComponent {
+export class TechnicianListComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
   ELEMENT_DATA: Technical[] = []
 
   displayedColumns: string[] = ['id', 'name', 'cpf', 'email', 'action'];
@@ -22,14 +27,10 @@ export class TechnicianListComponent {
     private service: TechnicalService
   ) {}
 
-  ngOnInit(): void {
-    this.findAll();
-  }
-
-  findAll() {
-    this.service.findAll().subscribe(response => {
-      this.ELEMENT_DATA = response;
-      this.dataSource = new MatTableDataSource<Technical>(response);
+  findAll () {
+    this.service.findAll().pipe().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Technical>(resposta);
       this.dataSource.paginator = this.paginator;
     })
   }
